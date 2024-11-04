9e84a1db3d9cb4079db8168ead6b7e0e59c03e14
Will Baxter <bax.will@googlemail.com>
2024-11-04 15:33:20 +0000

    Completed longest consecutive

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -1,4 +1,24 @@
-// Input: nums = [2,20,4,10,3,4,5]
+const testArr = [7, 2, 20, 4, 10, 3, 4, 5, 6];
 
-// Output: 4
+function longestConsecutive(nums) {
+  const numSet = new Set(nums);
+  let longestStreak = 0;
 
+  for (let num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+  }
+
+  return longestStreak;
+}
+
+console.log(longestConsecutive(testArr));
